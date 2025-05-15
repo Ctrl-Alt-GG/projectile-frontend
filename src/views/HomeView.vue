b8e2178ed0cd9236eae3244adeb1432c311cc3e5
marcsello <punkosdmarcell@rocketmail.com>
2025-05-15 23:55:40 +0200

    finished present view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -21,39 +21,7 @@
     <b-row class="mt-2">
       <b-col>
 
-        <b-card no-body v-for="gameServer in liveData.gameServers" class="my-2">
-          <b-card-body>
-            <b-container fluid>
-              <b-row align-v="center">
-                <b-col cols="auto" class="px-1">
-                  <img :src="getIcon(gameServer.game)" style="height: 2.8em; width: auto">
-                </b-col>
-                <b-col>
-                  <div><b>{{ gameServer.longName }}</b></div>
-                  <div v-if="gameServer.info" class="text-muted"><small>{{ gameServer.info }}</small></div>
-                </b-col>
-                <b-col class="text-center">{{ formatPlayerCount(gameServer) }}</b-col>
-                <b-col class="text-end">
-                  <code>{{ gameServer.address }}</code>
-                </b-col>
-              </b-row>
-
-              <b-row align-h="center" class="mt-3 pt-2" style="border-top: 1px solid #444"
-                     v-if="gameServer.meta.knowPlayers && gameServer.onlinePlayers.length > 0">
-                <b-col cols="12">
-                  <table style="width: 100%">
-                    <tr class="p-1" v-for="ply in gameServer.onlinePlayers">
-                      <td>{{ ply.name }}</td>
-                      <td class="text-muted">{{ ply.info }}</td>
-                      <td class="text-end" v-if="gameServer.meta.playersHasScore">{{ ply.score }}</td>
-                    </tr>
-                  </table>
-                </b-col>
-              </b-row>
-
-            </b-container>
-          </b-card-body>
-        </b-card>
+        <gameserver-card v-for="gameServer in liveData.gameServers" :gameServer="gameServer" class="my-2" />
 
       </b-col>
     </b-row>
@@ -71,7 +39,6 @@
 <script>
 import {inject} from 'vue'
 import NetworkErrorOverlay from "@/components/NetworkErrorOverlay.vue";
-import getIcon from "@/data/icons.js";
 
 export default {
   components: {NetworkErrorOverlay},
@@ -93,7 +60,6 @@ export default {
   },
 
   methods: {
-    getIcon,
     update() {
       this.api.get("/bundle").then(res => {
         this.liveData = res.data;
@@ -103,20 +69,13 @@ export default {
         this.networkError = true;
       })
     },
-    formatPlayerCount(game) {
-      if (game.meta.knowOnlinePlayerCount) {
-        return `${game.playerCount} / ${game.maxPlayers}`;
-      } else {
-        return `max: ${game.maxPlayers}`;
-      }
-    }
   },
 
   mounted() {
     this.update()
     this.interval = setInterval(() => {
       this.update()
-    }, 5000)
+    }, 10000)
   },
   unmounted() {
     if (this.interval !== null) {
--- src/views/PresentView.vue
+++ src/views/PresentView.vue
@@ -1,8 +1,26 @@
 <template>
   <network-error-overlay :active="networkError" />
-  <div>
-    present view
-  </div>
+  <b-container fluid class="border-0">
+    <b-row class="mt-2 mb-3">
+      <b-col class="text-center">
+        <h1>Ctrl-Alt-GG 2025</h1>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col class="my-2 mx-5">
+        <div class="announcement-text">
+          {{ liveData.announcement.text }}
+        </div>
+      </b-col>
+    </b-row>
+    <b-row class="my-2">
+      <b-col>
+        <b-card-group columns>
+          <gameserver-card v-for="gameServer in liveData.gameServers" :gameServer="gameServer" class="my-2 p-2" />
+        </b-card-group>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
@@ -45,6 +63,8 @@ export default {
     this.interval = setInterval(() => {
       this.update()
     }, 5000)
+
+    document.body.style.background = "#000"; // time constarint
   },
   unmounted() {
     if (this.interval !== null) {
@@ -54,6 +74,25 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.announcement-text {
+  font-size: 1.3em;
+  min-height: 2em;
+  white-space: pre-wrap;
+  line-height: 1.3em;
+}
 
+.container-fluid {
+  color: #fff !important;
+  font-size: 1.1em;
+}
+
+.card {
+  --bs-card-bg: #000 !important;
+  --bs-card-border-color: #666 !important;
+}
+
+.card-columns {
+    column-count: 2 !important;
+}
 </style>
